5ccc59aa39456e3f7b4440c7c56dee6982fe6103
adam_petera <ad.petera@gmail.com>
2020-10-28 19:52:03 +0100

    initial commit

---
+++ IWWW_SemestralniPrace_Petera/header.php
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <div class="logo">SALIBANDYSTORE.CZ</div>
+        <ul class="nav_links">
+            <li><a href="#">Domů</a></li>
+            <li>
+                <a href="#">Kontakt</a>
+                <ul>
+                    <li><a href="#">Formulář</a></li>
+                    <li><a href="#">Obecné info</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Účet</a>
+                <ul>
+                    <li><a href="#">Přihlášení</a></li>
+                    <li><a href="#">Registrace</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Košík</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ IWWW_SemestralniPrace_Petera/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>SalibandyStore</title>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="https://kit.fontawesome.com/cb337acf51.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+    include "header.php"
+?>
+
+<div class="wrapper">
+    <div class="sidebar">
+        <h2>Produkty  <i class="fas fa-shopping-cart"></i></h2>
+        <ul>
+            <li><a href="#"><i class="fas fa-check"></i>Florbalové hole</a></li>
+            <li><a href="#"><i class="fas fa-fan"></i>Čepele</a></li>
+            <li><a href="#"><i class="fas fa-bullseye"></i>Brankáři</a></li>
+            <li><a href="#"><i class="fas fa-suitcase-rolling"></i>Vaky, tašky</a></li>
+            <li><a href="#"><i class="far fa-futbol"></i>Míčky</a></li>
+            <li><a href="#"><i class="fas fa-glasses"></i>Doplňky</a></li>
+            <li><a href="#"><i class="fas fa-tshirt"></i>Sportovní oblečení</a></li>
+            <li><a href="#"><i class="fas fa-crosshairs"></i>Branky</a></li>
+        </ul>
+    </div>
+    <div class="main_content">
+        <div class="headerrr">Welcome</div>
+        <div class="info">
+            <div>Have fun!</div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
