1e138c966de1ef101403abf1c48c008caffd1fbd
adam_petera <ad.petera@gmail.com>
2020-10-28 20:12:21 +0100

    commit 2

--- IWWW_SemestralniPrace_Petera/index.php
+++ IWWW_SemestralniPrace_Petera/index.php
@@ -9,27 +9,18 @@
 </head>
 <body>
 <?php
-    include "header.php"
+include "header.php"
 ?>
 
 <div class="wrapper">
-    <div class="sidebar">
-        <h2>Produkty  <i class="fas fa-shopping-cart"></i></h2>
-        <ul>
-            <li><a href="#"><i class="fas fa-check"></i>Florbalové hole</a></li>
-            <li><a href="#"><i class="fas fa-fan"></i>Čepele</a></li>
-            <li><a href="#"><i class="fas fa-bullseye"></i>Brankáři</a></li>
-            <li><a href="#"><i class="fas fa-suitcase-rolling"></i>Vaky, tašky</a></li>
-            <li><a href="#"><i class="far fa-futbol"></i>Míčky</a></li>
-            <li><a href="#"><i class="fas fa-glasses"></i>Doplňky</a></li>
-            <li><a href="#"><i class="fas fa-tshirt"></i>Sportovní oblečení</a></li>
-            <li><a href="#"><i class="fas fa-crosshairs"></i>Branky</a></li>
-        </ul>
-    </div>
+    <?php
+        include "sidemenu.php"
+    ?>
     <div class="main_content">
-        <div class="headerrr">Welcome</div>
+        <div class="text">Welcome</div>
         <div class="info">
             <div>Have fun!</div>
+            <div>Dont hesitate to get in touch!</div>
         </div>
     </div>
 </div>
