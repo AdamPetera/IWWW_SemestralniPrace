291c51407a8e7b16c97bcaa1b1589bbbcb0b138e
adam_petera <ad.petera@gmail.com>
2020-10-29 16:56:48 +0100

    registration form added

--- IWWW_SemestralniPrace_Petera/sidemenu_transform.php
+++ IWWW_SemestralniPrace_Petera/sidemenu_transform.php
@@ -6,21 +6,21 @@
         <a href="#" title="Čepele"><i class="fas fa-fan"></i></a>
     </div>
     <div class="icon_box">
-        <a href="#"title="Brankáři"><i class="fas fa-bullseye"></i></a>
+        <a href="#" title="Brankáři"><i class="fas fa-bullseye"></i></a>
     </div>
     <div class="icon_box">
-        <a href="#"title="Vaky, tašky"><i class="fas fa-suitcase-rolling"></i></a>
+        <a href="#" title="Vaky, tašky"><i class="fas fa-suitcase-rolling"></i></a>
     </div>
     <div class="icon_box">
-        <a href="#"title="Míčky"><i class="far fa-futbol"></i></a>
+        <a href="#" title="Míčky"><i class="far fa-futbol"></i></a>
     </div>
     <div class="icon_box">
-        <a href="#"title="Doplňky"><i class="fas fa-glasses"></i></a>
+        <a href="#" title="Doplňky"><i class="fas fa-glasses"></i></a>
     </div>
     <div class="icon_box">
-        <a href="#"title="Oblečení"><i class="fas fa-tshirt"></i></a>
+        <a href="#" title="Oblečení"><i class="fas fa-tshirt"></i></a>
     </div>
     <div class="icon_box">
-        <a href="#" title="Branky"><i class="fas fa-crosshairs"></i></a>
+        <a href="#"  title="Branky"><i class="fas fa-crosshairs"></i></a>
     </div>
 </div>
