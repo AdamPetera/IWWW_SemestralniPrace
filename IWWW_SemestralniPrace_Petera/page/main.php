0e4cb1432c2e6b7e3ca50e059ed4ba1b714cb099
adam_petera <ad.petera@gmail.com>
2020-10-31 12:55:24 +0100

    login working

--- IWWW_SemestralniPrace_Petera/page/main.php
+++ IWWW_SemestralniPrace_Petera/page/main.php
@@ -1,3 +1,18 @@
 <?php
-    include "./slider_show.php";
+include "./slider_show.php";
 ?>
+<div class="most_wanted_items">
+    <h2>Žhavé položky</h2>
+    <article class="item_preview">
+        <h3>Florbalky</h3>
+        <img src="../images/itemprev_stick.jpg" alt="Florbalky">
+    </article>
+    <article class="item_preview">
+        <h3>Vaky, tašky</h3>
+        <img src="../images/itemprev_bag.jpg" alt="Vaky, tašky">
+    </article>
+    <article class="item_preview">
+        <h3>Brankáři</h3>
+        <img src="../images/itemprev_mask.png" alt="Brankáři">
+    </article>
+</div>
